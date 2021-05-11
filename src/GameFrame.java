a20c2f5322faa3b8ab231f33cb7eb523e8a1a91b
mPeltzerNovi <m.peltzer@novi-education.nl>
2021-05-11 10:24:19 +0200

    New input ActionListener problem solved

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -2,7 +2,17 @@ import javax.swing.*;
 
 public class GameFrame extends JFrame {
 
+    //Constructor
     GameFrame(){
 
+        GamePanel panel = new GamePanel();
+
+        this.add(panel);
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
     }
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,10 +1,73 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
+    //Variabelen declareren
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
+    static final int DELAY = 75;
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int applesEaten; //(begint op nul)
+    int appleX;
+    int appleY;
+    char direction = 'R'; //begint met naar rechts bewegen
+    boolean running = false;
+    Timer timer;
+    Random random;
 
+
+    //Constructor
     GamePanel(){
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.BLACK);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
+    }
+    //Methods:
+    public void startGame() {
+        newApple();
+        running = true;
+        timer = new Timer(DELAY, this); //nu doet hij het wel; vergeten om "implements ActionListener" aan R10 toe te voegen
+        timer.start();
+    }
+
+    public void paintComponent(Graphics g) {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
+    public void newApple() {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void checkApple() {
+
+    }
+
+    public void checkCollisions() {
+
+    }
+
+    public void gameOver(Graphics g) {
 
     }
 
@@ -12,5 +75,14 @@ public class GamePanel extends JPanel implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+
+    //Create inner class
+    public class MyKeyAdapter extends KeyAdapter {
+        //Omdat hier een methode in gaat, ga je dus een methode overriden; toevoegen @Override
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
     }
 }
